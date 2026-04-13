26538486f2e98c9575fa0b6b0cb836786b9cc0ea
Shubham Khandelwal <shubhamk@excollo.com>
2026-04-13 15:20:46 +0530

    fix: change admin name

--- my-react-app/src/pages/Escalations.jsx
+++ my-react-app/src/pages/Escalations.jsx
@@ -262,8 +262,8 @@ const Escalations = () => {
                )}
              </div>
              <div className="text-right hidden sm:block">
-                <p className="text-xs font-black text-slate-800 dark:text-slate-200">Alex Rivera</p>
-                <p className="text-[10px] text-slate-400 uppercase tracking-wider">Ops Manager</p>
+                <p className="text-xs font-black text-slate-800 dark:text-slate-200">Ramesh Kumar</p>
+                <p className="text-[10px] text-slate-400 uppercase tracking-wider">Admin</p>
               </div>
               <img alt="User profile" className="w-8 h-8 rounded-full border-2 border-slate-200 dark:border-slate-700 shadow-sm"
                 src="https://lh3.googleusercontent.com/aida-public/AB6AXuC8ov37m6Ru1jtLXavUm2Wv7-q8IqttbDcSU5OJzUCKT6ZmPdV8o10Gkm2bzBBlUkUAfR7nPEInOWhBPKK0JB-n56VPQC2sJvCZVr9a9eqzujzWSusoB7Pqo3Zl5PSfDCMpzoPbo0JZh5CHcjqc7lATQ1qKELXGJ7WeD5DB3SN3FaTJ4H9VBzP_Fvv51A3UPXtSYL_rtKoK2k8LRfiEklf60DY9c3Hul2Ue3yIjaHQmSa85wLfALExg-6xFvgM8lPDR6WQOIutN4I6d" />
